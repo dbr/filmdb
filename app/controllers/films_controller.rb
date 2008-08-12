b6be1c0987fb67f8cce966dc49987f5447325127
dbr <dbr.onix@gmail.com>
2008-08-12 20:10:34 +0100

    Added ?imdb=false to list only films without IMDB IDs, also start of pulling data from IMDB (currently to grab the correct film title, if accessed /film/myfilm?imdb_id_check=true )

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,8 +4,12 @@ class FilmsController < ApplicationController
   before_filter :authenticate, :except => [:index, :show]
 
   def index
-    @films = Film.search(params[:search])
-
+    @films = Film.order_title.find(:all)
+    @films = Film.order_title.has_imdb if params[:imdb] == "true"
+    @films = Film.order_title.no_imdb if params[:imdb] == "false"
+    
+    @films = Film.order_title.search( params[:search].strip ) if params[:search]
+    
     respond_to do |format|
       format.html # index.html.erb
       format.js # index.js
@@ -17,8 +21,22 @@ class FilmsController < ApplicationController
   def show
     begin
       @film = Film.find_by_permalink(params[:id])
+      raise ActiveRecord::RecordNotFound, "Film not found" if @film.nil?
+      
+      if params[:imdb_id_check]
+        @imdb_data = @film.imdb_id.nil? ? nil : Imdb.find_movie_by_id(@film.imdb_id)
+        render :layout => "films", :partial => "confirm_imdb_result"
+      end
+      
+      if params[:imdb_id_correct]
+        @imdb_data = @film.imdb_id.nil? ? nil : Imdb.find_movie_by_id(@film.imdb_id)
+        @film.title = @imdb_data.title
+        if @film.save
+          render :action => "show"
+        end
+      end
     rescue ActiveRecord::RecordNotFound
-      flash[:error] = "Unknown id..."
+      flash[:error] = "Unknown film..."
       redirect_to :action => "index"
       return
     end
