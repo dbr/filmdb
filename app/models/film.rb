327183c718c853096e51422e851d801f7686a9c2
dbr <dbr.onix@gmail.com>
2008-08-12 20:08:50 +0100

    Named scopes for with and without IMDB IDs. Make permalink be regenerated when the user edits the film (so the film name in the URL isn't outdated). Also tries to auto-fills blank IMDB ID using the Imdb plugin

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,7 +1,31 @@
 class Film < ActiveRecord::Base
+  before_save :auto_fill_imdb_id
+  before_save :regen_permalink
+  
   has_permalink :title 
   def to_param
     permalink
+  end
+  
+  named_scope :order_title, :order => 'title'
+  named_scope :has_imdb, :conditions => ['imdb_id LIKE "tt%%"']
+  named_scope :no_imdb, :conditions => ['imdb_id NOT LIKE "tt%%"']
+  
+  named_scope :search, lambda{ |*args| {:conditions => ['title LIKE ?', "%%#{args.first}%%" || '%%']}}
+  
+  
+  def regen_permalink
+    self.permalink = PermalinkFu.escape(self.title)
+  end
+  
+  def auto_fill_imdb_id
+    if imdb_id==""
+      begin
+        self.imdb_id = Imdb.find_movie_by_name(self.title).imdb_id || ""
+      rescue OpenURI::HTTPError
+        self.imdb_id = ""
+      end
+    end
   end
   
   def imdb_link
@@ -12,12 +36,4 @@ class Film < ActiveRecord::Base
     end
   end
   
-  def self.search(search)
-    if search
-      search.strip!
-      find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
-    else
-      find(:all)
-    end
-  end
 end
