b7d386aa03620822a96d896e6d60d7dbbafdcb55
dbr <dbr.onix@gmail.com>
2008-08-12 20:03:47 +0100

    Removed /stats URL, as it's now part of the main /films page

--- app/controllers/stats_controller.rb
+++
@@ -1,7 +0,0 @@
-class StatsController < ApplicationController
-  def index
-    @total = Film.find(:all).length
-    @without_imdb = Film.no_imdb.length
-    @per_complete = 100 - ((@without_imdb / @total.to_f) * 100).round(2)
-  end
-end
