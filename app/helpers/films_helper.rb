bf24ab7878b9186c19e7e1cb7d006584ba00f8f1
dbr <dbr.onix@gmail.com>
2008-08-12 20:04:43 +0100

    Added stat commands to films helper

--- app/helpers/films_helper.rb
+++ app/helpers/films_helper.rb
@@ -6,4 +6,15 @@ module FilmsHelper
       return "N/a"
     end
   end
+  
+  def imdb_id_completion
+    100 - ((num_without_imdb / num_all.to_f) * 100).round(2)
+  end
+  
+  def num_without_imdb
+    Film.no_imdb.length
+  end
+  def num_all
+    Film.find(:all).length
+  end
 end
